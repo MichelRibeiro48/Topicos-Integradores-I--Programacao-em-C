607c23f51922d557d56864a8c8d4af1787cb4cb9
Michel Ribeiro Alves <62553511+MichelRibeiro48@users.noreply.github.com>
2021-08-27 11:56:14 -0300

    Acrescentando Nome, Matricula e Turma

--- Problema4/Problema4.cpp
+++ Problema4/Problema4.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// aluno : Michel Ribeiro Alves
+// Matricula: 04058860
+// Turma: BCC-3NMA
 int main(){
 	int escolha,conta;
 	float saldo,vOperacao,cheque,saldonovo;
