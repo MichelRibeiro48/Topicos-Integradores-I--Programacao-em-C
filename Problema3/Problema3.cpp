c2adc6fdee7ba81b44d87be4d7f869070eff524b
Michel Ribeiro Alves <62553511+MichelRibeiro48@users.noreply.github.com>
2021-09-02 21:10:21 -0300

    Corrigindo Erros com Ricardo

--- Problema3/Problema3.cpp
+++ Problema3/Problema3.cpp
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// aluno : Michel Ribeiro Alves
-// Matricula: 04058860
-// Turma: BCC-3NMA
 int main(){
-	float ricardo, conta;
-	int joao, rodrigo;
+	float conta;
+	int joao, rodrigo, ricardo;
 	
 	printf("Informe a conta a ser dividida:\n");
 	scanf("%f", &conta);
-		
+	
+	ricardo = int(conta);
 	joao = (int)conta;
 	rodrigo = (int)conta;
-	printf("Valor do joao a pagar = %d | Valor do rodrigo a pagar = %d | Valor do ricardo a pagar = %.2f\n", joao / 3, rodrigo / 3, conta / 3);
+	conta -= ricardo;
+	printf("Valor do joao a pagar = %d | Valor do rodrigo a pagar = %d | Valor do ricardo a pagar = %.2f\n", joao / 3, rodrigo / 3,  (ricardo / 3) + conta);
 	
 	system("pause");
 }
